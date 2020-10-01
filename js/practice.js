172a686deda2e7b7a4f4482c55660d629d1cd72c
jj-pa <krap9482@gmail.com>
2020-10-01 15:57:37 +0900

    Add a current scene variable

--- js/practice.js
+++ js/practice.js
@@ -1,4 +1,8 @@
 (() => {
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -46,6 +50,29 @@
         }
     }
 
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+
+            if (currentScene < sceneInfo.length - 1) {
+                currentScene++;
+            }
+        }
+
+        if (yOffset < prevScrollHeight) {
+            if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스 방지 (모바일)
+            currentScene--;
+        }
+    }
+
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset; // 스크롤 Y 수치
+        scrollLoop();
+    });
     setLayout();
 })();
